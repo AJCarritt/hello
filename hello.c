6164c73e88a1d2f6b2726c7ea754db5deae1c215
AJCarritt <111910706+AJCarritt@users.noreply.github.com>
2022-08-26 15:29:36 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,1 +1,10 @@
-    printf("AJ Carritt");
+    #include <stdlib.h>
+    #include <stdio.h>
+
+    int main(int argc, char **argv) {
+
+    printf("AJ Carritt!");
+    printf("Electrical Engineering");
+
+    return 0;
+    }
