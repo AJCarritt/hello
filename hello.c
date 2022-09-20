020453c8b4fca6827e44991f0581d165e5d452b7
jshelman2 <111915337+jshelman2@users.noreply.github.com>
2022-09-20 19:19:51 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -3,8 +3,12 @@
 
     int main(int argc, char **argv) {
 
-    printf("AJ Carritt!");
-    printf("Electrical Engineering");
+    printf("AJ Carritt!\n");
+    printf("Electrical Engineering\n");
+    printf("Johnathan Shelman\n");
+    printf("Electrical Engineering\n");
+
+    // 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
 
     return 0;
     }
